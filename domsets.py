4f2dc9a240b58169ac87a01c4ce8a14f252c4c2a
osboxes.org <Chris>
2017-04-19 01:36:01 +0100

    Added node class

--- domsets.py
+++ domsets.py
@@ -2,16 +2,31 @@ from subprocess import call
 from sys import argv
 import csv
 
+class Node:
+	id=-1
+	neighborList=[]
+
 if len(argv) != 2:
 	print("Usage: python domsets.py inFile")
 	exit(0)
 script, inFile = argv
+
+
+nodes=[]
+numNodes, numEdges = 
+with open(inFile) as f:
+	next(f)
+	for line in f:
+		
+	
+
+# Get GDVs
+for line in open("counts.out"):
+	GDVs.append(line[:-1].split(" "))
 call(["./orca", "4", inFile, "counts.out"])
 GDVs=[]
 for line in open("counts.out"):
 	GDVs.append(line[:-1].split(" "))
-print GDVs[4][5]
-print GDVs[5][4]
-print len(GDVs)
-print len(GDVs[5])
+print GDVs
+
 
