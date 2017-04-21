fc2b35c5db9a0e5d6467394dc18da5c148d28607
bmarin <bmarin@nd.edu>
2017-04-21 16:44:52 -0400

    working on pruning, included de-duplicating script

--- prune.py
+++ prune.py
@@ -1,35 +1,70 @@
-def sortNodeList(node_list):
+def sortList(node_list):
 	'''
-	Description: Merge sorts in place a list of integers
+	Description: Merge sorts a list of integers
 	Input: list of integers
 	Ouput: None
 	'''
-	if len(alist) > 1:
-		mid = len(alist) // 2
-		lefthalf = alist[:mid]
-		righthalf = alist[mid:]
-		mergeSort(lefthalf)
-		mergeSort(righthalf)
+	if len(node_list) > 1:
+		mid = len(node_list) // 2
+		lefthalf = node_list[:mid]
+		righthalf = node_list[mid:]
+		sortNodeList(lefthalf)
+		sortNodeList(righthalf)
 
 		i=0
 		j=0
 		k=0
 		while i < len(lefthalf) and j < len(righthalf):
 			if lefthalf[i] < righthalf[j]:
-				alist[k] = lefthalf[i]
+				node_list[k] = lefthalf[i]
 				i = i+1
 			else:
-				alist[k] = righthalf[j]
+				node_list[k] = righthalf[j]
 				j = j+1
 			k = k+1
 
 		while i < len(lefthalf):
-			alist[k]=lefthalf[i]
+			node_list[k] = lefthalf[i]
 			i=i+1
 			k=k+1
 
 		while j < len(righthalf):
-			alist[k]=righthalf[j]
+			node_list[k] = righthalf[j]
+			j=j+1
+			k=k+1
+
+def sortNodes(node_list):
+	'''
+	Description: Merge sorts a list of Node objects
+	Input: list of Nodes
+	Ouput: None
+	'''
+	if len(node_list) > 1:
+		mid = len(node_list) // 2
+		lefthalf = node_list[:mid]
+		righthalf = node_list[mid:]
+		sortNodeList(lefthalf)
+		sortNodeList(righthalf)
+
+		i=0
+		j=0
+		k=0
+		while i < len(lefthalf) and j < len(righthalf):
+			if lefthalf[i].nodeID < righthalf[j].nodeID:
+				node_list[k].nodeID = lefthalf[i].nodeID
+				i = i+1
+			else:
+				node_list[k].nodeID = righthalf[j].nodeID
+				j = j+1
+			k = k+1
+
+		while i < len(lefthalf):
+			node_list[k].nodeID = lefthalf[i].nodeID
+			i=i+1
+			k=k+1
+
+		while j < len(righthalf):
+			node_list[k].nodeID = righthalf[j].nodeID
 			j=j+1
 			k=k+1
 	
@@ -51,30 +86,29 @@ def nodeInList(node_list, node_num):
 		if node_list[midpoint] == node_num:
 			return True
 		else:
-			if item < node_list[midpoint]:
+			if node_num < node_list[midpoint]:
 				last = midpoint - 1
 			else:
 				first = midpoint + 1	
 	return False
 
-def neigborsDominated(node_list, dom_set_list, node):
+def neighborsDominated(initial_node_list, node):
 	'''
 	Description: Determines whether all neighbors of given node are connected to a node in the
-	             dominating set
+	             dominating set or are dominating nodes themselves
 
-	Input: node_list - list of node objects currently in graph
-	       dom_set_list - integer list of node id's currently in dominating set
-	       node - node object to check the neihbors of
+	Input: initial_node_list - list of node objects in the orignal network
+	       node - node object to check the neighbors of
 
 	Output: returns true if all the neigbors of node_id are connected to the dominating set,
 		false otherwise
 	'''
 	for neighbor_node in node.neighborList:
-		if not nodeInList(dom_set_list, neighbor_node):
+		if not initial_node_list[neighbor_node].isDominated:
 			return False
 	return True
 
-def oneUndominatedNeighbor(node_list, dom_set_list, node):
+def oneUndominatedNeighbor(initial_node_list, node):
 	'''
 	Description: Determines whether all neighbors but one of given node are conected to a node
 		     in the dominating set
@@ -86,19 +120,19 @@ def oneUndominatedNeighbor(node_list, dom_set_list, node):
 	Output: returns true if all the neigbors but one of node_id are connected to the dominating set,
 		false otherwise
 	'''
-	oneConnected = 0
+	connectedNeighbors = 0
 	for neighbor_node in node.neighborList:
-		if node_list[neighbor_node].isDominated:
-			oneConnected += 1
-			if oneConnected > 1:
+		if initial_node_list[neighbor_node].isDominated:
+			connectedNeighbors += 1
+			if connectedNeighbors > 1:
 				return False
-	if oneConnected == 1:
+	if connectedNeighbors == 1:
 		return True
 	else:
 		return False
 
 
-def pruneGraph(node_list, dom_set_list):
+def pruneGraph(initial_node_list, current_node_list, dom_set):
 	'''
 	Description: Removes node from node_list if:
 			node is in dom_set_list
@@ -107,23 +141,27 @@ def pruneGraph(node_list, dom_set_list):
 			all its neighbors are connected to a node in dom_set_list
 				OR
 			node is connected to a node in dom_set_list AND 
-			exactly one neihbor is not connected to a node in dom_set_list
+			exactly one neighbor is not connected to a node in dom_set_list
 
-	Input: node_list - list of all node objects currently in graph
-	       dom_set_list - list of all node objects in the dominating set
+	Input: initial_node_list - list of node objects initially/globally in the graph
+	       current_node_list - list of all node objects currently in graph for this pruning round
+	       dom_set - set of all node objects in the dominating set
 
 	Ouput: node_list pruned according to the algorithm rules
 	'''
-	sortNodeList(node_list)
+	dom_set_list = list(dom_set)
+	sortList(dom_set_list)
+	sortNodes(current_node_list)
 	pruned_graph = []
-	for node in node_list:
-		if not nodeInList(dom_set_list, node.id):
+	for node in current_node_list:
+		if nodeInList(dom_set_list, node.nodeID):
 			continue
-		elif not neighborsDominated(node_list, dom_set_list, node):
+		elif node.isDominated and neighborsDominated(initial_node_list, node):
 			continue
-		elif not oneUndominatedNeighbor(node_list, dom_set_list, node):
+		elif node.isDominated and oneUndominatedNeighbor(initial_node_list, node):
 			continue
 		else:
 			pruned_graph.append(node)
 	return pruned_graph
-		
+
+
--- tests.py
+++ tests.py
@@ -1,6 +1,15 @@
 import unittest
 import prune
 
+class Node(object):
+	def __init__(self, nid):
+		self.nodeID=nid
+		self.neighborList=set()
+		self.isDominating=0
+		self.isDominated=0
+
+
+
 class TestPruningFunctions(unittest.TestCase):
 	def testSortNodeList(self):
 		list1 = []
@@ -9,39 +18,91 @@ class TestPruningFunctions(unittest.TestCase):
 		list3 = [1, 0, 3, 4]
 		list3_c  = [0, 1, 3, 4]
 		list4 = [1, 2, 3, 4]
-		self.assertEqual(prune.sortNodeList(list1), list1)
-		self.assertEqual(prune.sortNodeList(list2), list2_c)
-		self.assertEqual(prune.sortNodeList(list3), list3_c)
-		self.assertEqual(prune.sortNodeList(list4), list4_c)
+		list4_c = [1, 2, 3, 4]
+
+		prune.sortNodeList(list1)
+		prune.sortNodeList(list2)
+		prune.sortNodeList(list3)
+		prune.sortNodeList(list4)
+		self.assertEqual(list1, [])
+		self.assertEqual(list2, list2_c)
+		self.assertEqual(list3, list3_c)
+		self.assertEqual(list4, list4_c)
 	
 	def testBinarySearch(self):
 		list1 = [-4, -2, 0, 1, 4, 15, 32]
 		list2 = [1, 2, 4, 5, 6]
 		list3 = []
 		list4 = [0, 1, 2, 3, 4, 8, 9]
-		self.assertTrue(self.nodeInList(list1,-2))
-		self.assertTrue(self.nodeInList(list1, 0))
-		self.assertTrue(self.nodeInList(list1, 15))
-		self.assertTrue(self.nodeInList(list2,1))
-		self.assertTrue(self.nodeInList(list2,6))
-		self.assertTrue(self.nodeInList(list2,4)
-		self.asserTrue(self.nodeInList(list4,0))
-		self.assertFalse(self.nodeInList(list1,72))
-		self.assertFalse(self.nodeInList(list1,3))
-		self.assertFalse(self.nodeInList(list1,-90))
-		self.assertFalse(self.nodeInList(list2,0))
-		self.assertFalse(self.nodeInList(list2,3))
-		self.assertFalse(self.nodeInList(list2,71))
-		self.assertFalse(self.nodeInList(list3,72))
-		self.assertFalse(self.nodeInList(list4,7))
-		self.assertFalse(self.nodeInList(list4,72))
-		self.assertFalse(self.nodeInList(list4,-90))
-	
+		self.assertTrue(prune.nodeInList(list1,-2))
+		self.assertTrue(prune.nodeInList(list1, 0))
+		self.assertTrue(prune.nodeInList(list1, 15))
+		self.assertTrue(prune.nodeInList(list2,1))
+		self.assertTrue(prune.nodeInList(list2,6))
+		self.assertTrue(prune.nodeInList(list2,4))
+		self.assertTrue(prune.nodeInList(list4,0))
+		self.assertFalse(prune.nodeInList(list1,72))
+		self.assertFalse(prune.nodeInList(list1,3))
+		self.assertFalse(prune.nodeInList(list1,-90))
+		self.assertFalse(prune.nodeInList(list2,0))
+		self.assertFalse(prune.nodeInList(list2,3))
+		self.assertFalse(prune.nodeInList(list2,71))
+		self.assertFalse(prune.nodeInList(list3,72))
+		self.assertFalse(prune.nodeInList(list4,7))
+		self.assertFalse(prune.nodeInList(list4,72))
+		self.assertFalse(prune.nodeInList(list4,-90))
+
+	'''
+	All the following tests use a grpah that looks like
+	0 -- 1 -- 2 -- 3 -- 4 .... -- 10
+	'''
 	def testNeighborsDominated(self):
-		node_list = [1, 2, 3, 
+		node_list = [] 
+		node_list.append(Node(0))
+		node_list[0].neighborList.add(1)
+		for indx in range(1,10):
+			node_list.append(Node(indx))
+			node_list[indx].neighborList.add(indx + 1)
+			node_list[indx].neighborList.add(indx - 1)
+		node_list.append(Node(10))
+		node_list[10].neighborList.add(9)
+		dom_set_list = [1,3,5]
+		self.assertTrue(prune.neighborsDominated(node_list, dom_set_list, node_list[2]))
+		self.assertTrue(prune.neighborsDominated(node_list, dom_set_list, node_list[4]))
+		self.assertFalse(prune.neighborsDominated(node_list, dom_set_list, node_list[7]))
+		self.assertFalse(prune.neighborsDominated(node_list, dom_set_list, node_list[6]))
 
-)
-
+	def testOneUndominatedNeighbor(self):
+		node_list = [] 
+		node_list.append(Node(0))
+		node_list[0].neighborList.add(1)
+		for indx in range(1,10):
+			node_list.append(Node(indx))
+			node_list[indx].neighborList.add(indx + 1)
+			node_list[indx].neighborList.add(indx - 1)
+		node_list.append(Node(10))
+		node_list[10].neighborList.add(9)
+		dom_set_list = [1,3,5]
+		self.assertTrue(prune.oneUndominatedNeighbor(node_list, dom_set_list, node_list[6]))
+		self.assertTrue(prune.oneUndominatedNeighbor(node_list, dom_set_list, node_list[0]))
+		self.assertFalse(prune.oneUndominatedNeighbor(node_list, dom_set_list, node_list[7]))
+		self.assertFalse(prune.oneUndominatedNeighbor(node_list, dom_set_list, node_list[2]))
+	
+	def testPruneGraph(self):
+		node_list = [] 
+		node_list.append(Node(0))
+		node_list[0].neighborList.add(1)
+		for indx in range(1,10):
+			node_list.append(Node(indx))
+			node_list[indx].neighborList.add(indx + 1)
+			node_list[indx].neighborList.add(indx - 1)
+		node_list.append(Node(10))
+		node_list[10].neighborList.add(9)
+		dom_set_list = [1,3,5]
+		new_graph = []
+		for i in range(7, 11):
+			new_graph.append(Node(i))
+		self.assertEqual(prune.pruneGraph(node_list, dom_set_list), new_graph)
 
 if __name__ == '__main__':
 	unittest.main()
